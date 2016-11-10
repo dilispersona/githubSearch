c11195d94ac02ad45faded836ccf02e11209b512
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-10 15:44:53 +0530

    created asmple profile component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProfileComponent }  from './components/profile.component';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
